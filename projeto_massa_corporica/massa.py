a61ef589c1a37656a9385b8f076f2042585774fb
Carlos Dantas <carlos.d.dantas6@aluno.senai.br>
2025-09-12 17:27:50 -0300

    ADD:projetos finalizados

--- projeto_massa_corporica/massa.py
+++ projeto_massa_corporica/massa.py
@@ -1,46 +1,78 @@
 from google import genai
 import ttkbootstrap as ttk
 
-
 class Janela_chat():
     def __init__(self):
 
-        self.janela = ttk.Window(themename="solar",
-                            title="TESTE DE MASSA CORPORICA")
-        self.janela.geometry("800x600+80+50")
+
+        #Janela
+        self.janela = ttk.Window(themename="darkly",
+                                   title="TESTE DE MASSA CORPORAL")
+        self.janela.geometry("800x400+80+50")
         self.janela.resizable(False, False)
+        self.create_widgets()
 
-        #Ira adicionar uma texto
-        self.txt = ttk.Label(self.janela, text="DIGITE SEU PESO E SUA ALTURA")
-        self.txt.pack(pady=20)
+    def create_widgets(self):
+        self.label_titulo = ttk.Label(self.janela, text="DIGITE SEU PESO E SUA ALTURA AQUI")
+        self.label_titulo.pack(pady=20)
+
+        # Campos de entrada para peso e altura
+        self.campos = ttk.Frame(self.janela)
+        self.campos.pack(pady=10)
+
+        self.label_peso = ttk.Label(self.campos, text="Peso:") #Caixa 01 peso
+        self.label_peso.pack(side="left", padx=10)
+
+        self.entry_peso = ttk.Entry(self.campos, width=5) #Largura da caixa
+
+        self.entry_peso.pack(side="left", padx=10) 
 
 
-        self.PS = ttk.Label(self.janela, text="DIGITE SEU PESO AQUI")
-        self.PS.pack(pady=20)
-        self.alt = ttk.Label(self.janela, text="DIGITE SUA ALTURA AQUI")
-        self.alt.pack(pady=20)
+        self.label_altura = ttk.Label(self.campos, text="Altura:") #02 altura 
+        self.label_altura.pack(side="left", padx=10)
 
+        self.entry_altura = ttk.Entry(self.campos, width=5)
+        self.entry_altura.pack(side="left", padx=10)
 
-        self.campo_nome = ttk.Entry(self.janela,
-                                    width=5)
-        self.campo_nome.place(x=10, y=10)
-
-        self.pes_nome_nome = ttk.Entry(self.janela,
-                                    width=5
-                                    )
+        # Botão 
+        self.button_calcular = ttk.Button(self.janela,
+                                           text="CALCULAR IMC", 
+                                           command=self.calcular_imc)
+        self.button_calcular.pack(pady=20)
         
-        self.pes_nome_nome.pack(pady=10,)
+        # Label para exibir o resultadoO
+        self.label_resultado = ttk.Label(self.janela, text="")
+        self.label_resultado.pack(pady=20)
 
-        self.label_resultado = ttk.Text(width=100, wrap="word",
-                                        state="disabled",)
-        self.label_resultado.pack()
-
+    def calcular_imc(self):
+        try:
+            peso = float(self.entry_peso.get())
+            altura = float(self.entry_altura.get())
+            imc = peso / (altura ** 2)
+        
+            if altura == 0:
+                self.label_resultado.config(text="ALTURA INVALIDA")
+                return
+            if imc < 18.5:
+                diagnostico = "Abaixo do peso"
+            elif 18.5 <= imc < 24.9:
+                diagnostico = "Peso normal"
+            elif 30 <= imc < 34.9:
+                diagnostico = "Obesidade Grau 1 "
+            elif 35 <= imc < 39.9:
+                diagnostico = "Obesidade Grau 2"
+            elif imc > 40:
+                diagnostico = "Obesidade Morbida"    
+            resultado_texto = (f"Seu IMC é {imc}, {diagnostico}")
+            
+            self.label_resultado.config(text=resultado_texto)
+            print(resultado_texto)
+        except:
+            self.label_resultado.config(text="Coloque apenas números.") # Trata o erro se não for números
 
     def run(self):
-            self.janela.mainloop()
+        self.janela.mainloop()
 
 if __name__ == "__main__":
-        chat = Janela_chat()
-        chat.run()
-
-      
+    app = Janela_chat()
+    app.run()
