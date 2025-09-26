b3ce94c4d5aaa3a1d6e9e2b28bfcd944c4aba6ed
Carlos Dantas <carlos.d.dantas6@aluno.senai.br>
2025-09-26 11:28:09 -0300

    ADD: LISTA TAREFAS ADC

--- 05_lista_tarefas/classe_lista_tarefa.py
+++ 05_lista_tarefas/classe_lista_tarefa.py
@@ -1,4 +1,6 @@
 import ttkbootstrap as tk
+from tkinter import Listbox, END
+from tkinter import Messagebox
 
 
 class janela_Lista_Tarefas():
@@ -7,16 +9,69 @@ class janela_Lista_Tarefas():
   def __init__(self):
 
     self.janela = tk.Window(themename='vapor')
-    self.janela.title("Lista Tarefas")
+    self.janela.title("LISTA TAREFAS")
     self.janela.geometry("1200x900+80+50")
 
     #Texto Adicionar
-    self.label_usu = tk.Label(text="Adicionar Tarefas") #Caixa 01 peso
-    self.label_usu.pack(padx=10)
+    self.tarefas = tk.Label(text="LISTA TAREFAS",
+                            style="danger",
+                            font=("Arial", 24)).pack(pady=20)
 
-    self.entry_usu = tk.Entry(width=25,
-                                ) #Largura da caixa
-    self.entry_usu.pack(padx=10) 
+    self.frame_add = tk.Frame(self.janela)
+    self.frame_add.pack(fill="x",padx=50)
+
+    self.add_tarefa = tk.Entry(self.frame_add)
+    self.add_tarefa.pack(side="left", fill="x", expand=True)
+
+    tk.Button(self.frame_add, text="ADICIONAR", cursor="hand2", command=self.adicionar_tarefa).pack(side="right", padx=10)
+
+    self.lista = Listbox(self.janela, font=("Segoe UI", 12), height=10)
+    self.lista.pack(padx=20,pady=20,fill='both', expand=True)
+
+                         
+    frame_botao = tk.Frame(self.janela)
+    frame_botao.pack(side="bottom", 
+                     padx=20, 
+                     fill="both")
+
+    botao_excluir = tk.Button(frame_botao, 
+                              text="Excluir", 
+                              style="Danger", command=self.excluir_tarefa)
+    
+    botao_excluir.pack(side="left", padx=20)
+
+    botao_marcar = tk.Button(frame_botao, 
+                             text="marcar como concluido", 
+                             width=30, 
+                             style="primary",
+                             command=self.concluir_tarefa)
+    
+    botao_marcar.pack(side="right", padx=20)
+
+
+  def adicionar_tarefa(self):
+     #pegango o texto da caixa de texto
+     tarefa = self.add_tarefa.get()
+
+     self.lista.insert(0, tarefa)
+
+  def excluir_tarefa(self):
+
+      item_selecionado = self.lista.curselection()
+      self.lista.delete(item_selecionado[0])
+     
+
+  def concluir_tarefa(self):
+     
+      item_concluido = self.lista.curselection()
+      self.lista.itemconfig(item_concluido, {"fg": "red", "selectforeground": "blue"})
+
+
+  def confirmar_saida():
+    resposta = messagebox.askyesno("Confirmação", "Deseja realmente sair?")
+    if resposta:
+        self.janela.destroy()
+
 
   def run(self):
     self.janela.mainloop()
--- 05_lista_tarefas/tela_jogo.py
+++ 05_lista_tarefas/tela_jogo.py
@@ -3,7 +3,8 @@ import ttkbootstrap as ttk
 from ttkbootstrap.dialogs import Messagebox
 from classe_lista_tarefa import janela_Lista_Tarefas
 
-class Janela_chat():
+class Janela():
+
     def __init__(self):
 
         #Janela
@@ -11,9 +12,7 @@ class Janela_chat():
                                    title="LOGIN")
         self.janela.geometry("800x400+80+400")
         self.janela.resizable(False, False)
-        self.create_widgets()
 
-    def create_widgets(self):
         self.label_titulo = ttk.Label(self.janela, text="FAÇA SEU LOGIN PARA AVANÇAR")
         self.label_titulo.pack(pady=20)
 
@@ -24,44 +23,43 @@ class Janela_chat():
         self.label_usu = ttk.Label(self.campos, text="USUÁRIO") #Caixa 01 peso
         self.label_usu.pack(padx=10)
 
-        self.entry_usu = ttk.Entry(self.campos, width=25,
+        self.nova_usu = ttk.Entry(self.campos, width=25,
                                     ) #Largura da caixa
-        self.entry_usu.pack(padx=10) 
+        self.nova_usu.pack(padx=10) 
 
 
         self.label_senha = ttk.Label(self.campos, text="SENHA") #02 altura 
         self.label_senha.pack(padx=10)
 
-        self.entry_senha = ttk.Entry(self.campos, width=25) #Largura da caixa
-        self.entry_senha.pack(padx=10)
-        
-
+        self.nova_senha = ttk.Entry(self.campos, width=25) #Largura da caixa
+        self.nova_senha.pack(padx=10)
 
         # Botão 
-        self.button_avancar = ttk.Button(self.janela,
+        self.botao_avancar = ttk.Button(self.janela,
                                            text="LOGAR",
                                            command= self.login
                                            )
         
-        self.button_avancar.place(y=200,x=320)
+        self.botao_avancar.place(y=200,x=320)
+
         #BOTÃO 02
-        self.button_avancar2 = ttk.Button(self.janela,
+        self.botao_avancar2 = ttk.Button(self.janela,
                                            text="SAIR",
                                            command=self.sair)
         
-        self.button_avancar2.place(y=200,x=410)
+        self.botao_avancar2.place(y=200,x=410)
 
 
-        self.button_redefinir = ttk.Button(self.janela,
+        self.botao_redefinir = ttk.Button(self.janela,
                                            text="ESQUECI MINHA SENHA",
                                            command= self.redefinir
                                            )
         
-        self.button_redefinir.place(y=250,x=315)
+        self.botao_redefinir.place(y=250,x=315)
 
     def login(self):
-        self.resposta_usuario = self.entry_usu.get()
-        self.resposta_senha = self.entry_senha.get()
+        self.resposta_usuario = self.nova_usu.get()
+        self.resposta_senha = self.nova_senha.get()
 
         if self.resposta_usuario == "1" and self.resposta_senha == "1":
             #Messagebox.show_info(message="Login feito com Sucesso!", title="Sucesso", alert=True)
@@ -82,7 +80,7 @@ class Janela_chat():
 
         self.janela.resizable(True, True)
 
-        self.redefinir = ttk.Window(themename="journal",
+        self.redefinir = ttk.Window(themename="vapor",
                                     title="REDEFINIR SENHA")
         self.redefinir.geometry("800x400+80+400")
         self.redefinir.resizable(False, False)
@@ -93,21 +91,21 @@ class Janela_chat():
         self.label_usu2 = ttk.Label(self.campos, text="NOVO USUARIO") #Caixa 01 peso
         self.label_usu2.pack(padx=10)
 
-        self.entry_usu2 = ttk.Entry(self.campos, width=25,
+        self.nova_usu2 = ttk.Entry(self.campos, width=25,
                                     ) #Largura da caixa
-        self.entry_usu2.pack(padx=10) 
+        self.nova_usu2.pack(padx=10) 
 
         self.label_senha2 = ttk.Label(self.campos, text="NOVA SENHA") #02 altura 
         self.label_senha2.pack(padx=10)
 
-        self.entry_senha2 = ttk.Entry(self.campos, width=25) #Largura da caixa
-        self.entry_senha2.pack(padx=10)
+        self.nova_senha2 = ttk.Entry(self.campos, width=25) #Largura da caixa
+        self.nova_senha2.pack(padx=10)
 
-        self.button_redefinir2 = ttk.Button(self.redefinir,
+        self.botao_redefinir2 = ttk.Button(self.redefinir,
                                            text="CONCLUIR",
                                            command= self.redefinir
                                            )
-        self.button_redefinir2.place(y=100,x=400)
+        self.botao_redefinir2.place(y=100,x=400)
         
         self.label_senha2 = self.label_senha2.get()
         self.resposta_usuario.set(self.label_senha2) # O texto do widget Label será atualizado
@@ -122,7 +120,7 @@ class Janela_chat():
         self.janela.mainloop()
 
 if __name__ == "__main__":
-    app = Janela_chat()
+    app = Janela()
     app.run()
 
 
